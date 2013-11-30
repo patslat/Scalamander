3773c0852ee85a84b6a05ee98631e0d97e37d953
patslat <slattery.patrickj@gmail.com>
2013-11-30 11:49:33 -0800

    add fnOfFn

--- Basics/higher_order_functions.scala
+++ Basics/higher_order_functions.scala
@@ -9,6 +9,14 @@ object HigherOrderFunctions {
   def prodFn(f: Int => Int, start: Int, stop: Int): Int =
     if (stop - start < 0) 1 else f(start) * prodFn(f, start + 1, stop)
 
+  def fnOfFn(acc: (Int, Int) => Int, fn: Int => Int, id: Int, start: Int, stop: Int): Int =
+    if (stop - start < 0)
+      id
+    else
+      acc(
+        fn(start),fnOfFn(acc, fn, id, start + 1, stop)
+      )
+
   def main(args: Array[String]) {
     println(sumInts(1, 5))
 
@@ -19,11 +27,42 @@ object HigherOrderFunctions {
       if (x == 0) 1 else x * fact(x - 1)
 
     println(sumFn(id, 5, 5))
+    println(sumFn(x => x, 5, 5))
+
     println(sumFn(square, 5, 5))
+    println(sumFn(x => x*x, 5, 5))
+
     println(sumFn(cube, 5, 5))
+    println(sumFn(x => x*x*x, 5, 5))
+
     println(sumFn(fact, 2, 2))
+
     println(prodFn(id, 1, 2))
+    println(prodFn(x => x, 1, 2))
     println(prodFn(square, 1, 5))
+    println(prodFn(x => x*x, 1, 5))
     println(prodFn(cube, 1, 5))
+
+    println(prodFn(x => x, 1, 3))
+
+    println(
+      fnOfFn(
+        (x: Int, y: Int) => (x + y),
+        (x:Int) => x,
+        0,
+        1,
+        3
+      )
+    )
+
+    println(
+      fnOfFn(
+        (x: Int, y: Int) => (x * y),
+        (x: Int) => x * x * x,
+        1,
+        1,
+        5
+      )
+    )
   }
 }
