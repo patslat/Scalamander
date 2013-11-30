5fa7f34b804acedb4c85953cb126d9fb09bb5705
patslat <slattery.patrickj@gmail.com>
2013-11-30 10:16:16 -0800

    add prodFn

--- Basics/curry.scala
+++ Basics/curry.scala
@@ -6,16 +6,24 @@ object Curry {
   def sumFn(f: Int => Int, a: Int, b: Int) =
     f(a) + f(b)
 
+  def prodFn(f: Int => Int, start: Int, stop: Int): Int =
+    if (stop - start < 0) 1 else f(start) * prodFn(f, start + 1, stop)
+
   def main(args: Array[String]) {
     println(sumInts(1, 5))
 
-    def square(x: Int): Int = x * x
-    def cube(x: Int): Int = x * x * x
+    def id(x: Int) = x
+    def square(x: Int) = x * x
+    def cube(x: Int) = x * x * x
     def fact(x: Int): Int =
       if (x == 0) 1 else x * fact(x - 1)
+
+    println(sumFn(id, 5, 5))
     println(sumFn(square, 5, 5))
     println(sumFn(cube, 5, 5))
     println(sumFn(fact, 2, 2))
-
+    println(prodFn(id, 1, 2))
+    println(prodFn(square, 1, 5))
+    println(prodFn(cube, 1, 5))
   }
 }
