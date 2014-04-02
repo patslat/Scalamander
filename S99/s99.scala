320b32e0c2b0fb6bd5c19c143b840ba15467d97d
Patrick Slattery <patrick@pagerduty.com>
2014-04-01 18:29:47 -0700

    add exercises to S99 up to P08

--- S99/s99.scala
+++ S99/s99.scala
@@ -1,50 +1,74 @@
 object Main {
   def main(args: Array[String]) {
     println("Hello, world!")
-    val list = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val ls = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    println("P01) This should be 9: " + last(list))
-    println("P02) This should be 8: " + penultimate(list))
-    println("p03) This should be 4: " + nth(3, list))
-    println("p04a) This should be 9: " + length(list))
-    println("p04b) This should be 9: " + recLength(list))
-    println("p05) This should be List(9, 8, 7, 6, 5, 4, 3, 2, 1): " + reverse(list))
+    println("P01) This should be 9: " + last(ls))
+    println("P02) This should be 8: " + penultimate(ls))
+    println("P03) This should be 4: " + nth(3, ls))
+    println("P04a) This should be 9: " + length(ls))
+    println("P04b) This should be 9: " + recLength(ls))
+    println("P05) This should be List(9, 8, 7, 6, 5, 4, 3, 2, 1): " + reverse(ls))
+    println("P06b) This should be true: " + isPalindrome("amanaplanacanalpanama".toList))
+    println("P06b) This should be false: " + isPalindrome("derp".toList))
+    println("P07a) This should be List(1, 2, 3, 4, 5, 6, 7, 8, 9): " + flatten(List(List(1, List(2, List(3))), List(4), 5, 6, List(List(List(7), List(8), List(9))))))
+    println("P07b) This should be List(1, 2, 3, 4, 5, 6, 7, 8, 9): " + flattenEZ(List(List(1, List(2, List(3))), List(4), 5, 6, List(List(List(7), List(8), List(9))))))
+    println("P08) This should be List('a, 'b, 'c, 'a, 'd, 'e): " + compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
   }
 
   // P01
-  def last[A](list: List[A]): A = list match {
+  def last[A](ls: List[A]): A = ls match {
     case h :: Nil  => h
     case _ :: tail => last(tail)
     case _         => throw new NoSuchElementException
   }
 
   // P02
-  def penultimate[A](list: List[A]): A = list match {
+  def penultimate[A](ls: List[A]): A = ls match {
     case h :: _ :: Nil => h
     case _ :: tail     => penultimate(tail)
     case _             => throw new NoSuchElementException
   }
 
   // P03
-  def nth[A](index: Int, list: List[A]): A = (index, list) match {
+  def nth[A](index: Int, ls: List[A]): A = (index, ls) match {
     case (0, h :: _   ) => h
     case (i, h :: rest) => nth(i - 1, rest)
     case (_, Nil      ) => throw new NoSuchElementException
   }
 
   // P04
-  def recLength[A](list: List[A]): Int = list match {
+  def recLength[A](ls: List[Any]): Int = ls match {
     case Nil       => 0
     case _ :: rest => 1 + recLength(rest)
     case _         => throw new NoSuchElementException
   }
+  def length[A](ls: List[Any]): Int = ls.foldLeft(0) { (count, _) => count + 1 }
 
-  def length[A](list: List[A]): Int = {
-    list.foldLeft(0) { (count, _) => count + 1 }
-  }
-
-  def reverse[A](list: List[A]): List[A] = list match {
+  // P05
+  def reverse[A](ls: List[Any]): List[Any] = ls match {
     case h :: Nil      => List(h)
     case h :: rest     => reverse(rest) ::: List(h)
+  }
+
+  // P06
+  def isPalindrome[A](ls: List[Any]): Boolean = ls == reverse(ls)
+
+  // P07
+  def flatten[A](ls: List[Any]): List[Any] = ls match {
+    case (h: List[_]) :: Nil  => flatten(h)
+    case h :: Nil             => List(h)
+    case (h: List[_]) :: rest => flatten(h) ::: flatten(rest)
+    case h :: rest            => List(h) ::: flatten(rest)
+  }
+  def flattenEZ(ls: List[Any]): List[Any] = ls flatMap {
+    case nestedList: List[_] => flatten(nestedList)
+    case h                   => List(h)
+  }
+
+  // P08
+  def compress(ls: List[Any]): List[Any] = ls.foldRight(List[Any]()) { (item, uniques) =>
+    if (uniques.isEmpty || uniques.head != item) item :: uniques
+    else uniques
   }
 }
